bf31743f17e974a3351c4aec3f805591577a62d2
Lucas Santos <lucascarvalhosantos91@gmail.com>
2024-07-29 00:14:49 -0300

    .

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const Build = std.Build;
+
+pub fn build(
+    b: *Build,
+) void {
+    const stbi = b.addModule("stbi", .{
+        .root_source_file = b.path("src/stbi.zig"),
+    });
+
+    const stbiDep = b.dependency("stbi", .{});
+    stbi.addIncludePath(stbiDep.path(&.{}));
+    const implName = "stbiImpl.c";
+    const write = b.addWriteFile(implName,
+        \\#define STB_IMAGE_IMPLEMENTATION
+        \\#include "stb_image.h"
+    );
+    stbi.addCSourceFile(.{
+        .file = write.getDirectory().path(b, implName),
+        .flags = &.{},
+    });
+    stbi.link_libc = true;
+}
